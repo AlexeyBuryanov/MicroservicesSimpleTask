d0dc9eb88f1521dec500ed63447a768f7402e0b7
AlexeyBuryanov <alexeyburyanov@gmail.com>
2019-01-24 14:43:58 +0300

    Add information logs in UsersStorageService

--- MicroservicesTask/ManagementService/Services/UsersStorage/UsersStorageService.cs
+++ MicroservicesTask/ManagementService/Services/UsersStorage/UsersStorageService.cs
@@ -2,6 +2,7 @@
 using ManagementService.Models;
 using ManagementService.Services.Request;
 using ManagementService.Settings;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -13,17 +14,22 @@ namespace ManagementService.Services.UsersStorage
     {
         private readonly ApiEndPoints _apiEndPoints;
         private readonly IRequestService _requestService;
+        private readonly ILogger _logger;
 
         public UsersStorageService(
             IOptionsMonitor<AppSettings> options,
-            IRequestService requestService)
+            IRequestService requestService,
+            ILogger<UsersStorageService> logger)
         {
             _apiEndPoints = options.CurrentValue.ApiEndPoints;
             _requestService = requestService;
+            _logger = logger;
         }
 
         public Task<IEnumerable<User>> GetUsersAsync()
         {
+            _logger.LogInformation("--- GetUsersAsync()");
+
             var builder = new UriBuilder(_apiEndPoints.UsersStorageService);
             builder.AppendToPath("Users");
             builder.AppendToPath("list");
@@ -35,6 +41,8 @@ namespace ManagementService.Services.UsersStorage
 
         public Task<User> AddOrReplaceUserAsync(User user)
         {
+            _logger.LogInformation($"--- AddOrReplaceUserAsync(User ID: {user.UserId})");
+
             var builder = new UriBuilder(_apiEndPoints.UsersStorageService);
             builder.AppendToPath("Users");
             builder.AppendToPath("update");
@@ -47,6 +55,8 @@ namespace ManagementService.Services.UsersStorage
 
         public async Task DeleteUserAsync(User user)
         {
+            _logger.LogInformation($"--- DeleteUserAsync(User ID: {user.UserId})");
+
             var builder = new UriBuilder(_apiEndPoints.UsersStorageService);
             builder.AppendToPath("Users");
             builder.AppendToPath("delete");
